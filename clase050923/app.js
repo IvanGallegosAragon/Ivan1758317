af5c52b4feb2eef54322408495346b63b3dd5761
IvanGallegosAragon <89938838+IvanGallegosAragon@users.noreply.github.com>
2023-09-08 15:14:04 -0600

    Add files via upload

---
+++ clase050923/app.js
@@ -0,0 +1,65 @@
+// -----------------------------------App1---------------------------------------------
+// JS es sencitivo
+// siempre acaba con punto y coma
+// alert es minuscula, palabra reservada
+// alert("Hola1"); 
+// alert("Hola2");
+// alert("Hola3");
+console.log("Hola desde consola");
+// los "" son tipo text
+// existen varios
+console.log(20);
+console.log(20.2);
+console.log(1,2);
+console.log(1+1);
+console.log(true);
+console.log(false);
+
+// ------------------------------------App2------------------------------------------------
+
+// Variables:
+// declarar variables
+// Con var inicializamos la varaible y la podremos renombrar multiples veces
+var nombre = "Juan";
+console.log(nombre);
+var nombre = "Raul";
+console.log(nombre);
+
+//let no permite reinicializar
+let user = "PayasoMaligno";
+console.log(user);
+
+
+// const
+// constante de una sola lectura
+const pi = 3.15;
+console.log(pi);
+
+//Asignación a una variable con un =
+// ---------------------------------------------------App3------------------------------
+// let num3 = parseInt(prompt("Ingresa un numero"));
+let num1 = 5;
+let num2 = 5;
+
+// Suma
+console.log(num1+num2);
+// Resta
+console.log(num1-num2);
+// multi
+console.log(num1*num2);
+// Div
+console.log(num1/num2);
+
+// ---------------------------------------------------App4
+//Actividad
+let nombreUser = "Luis";
+let edad = 54;
+let nacionalidad = "Mexicana";
+let 
+
+
+
+
+
+
+
